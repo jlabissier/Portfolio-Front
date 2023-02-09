9a34e29a39666947265d1dbb465098dbd47b18ca
jlabissier <julianlabissier@gmail.com>
2023-02-09 18:09:40 -0300

    arreglo extras view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import Extras from '../components/Extras.vue';
+import Extras from '../views/ExtrasView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
